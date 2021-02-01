3501e229cb5ddc0e007f1b55fb8f6633209e5004
Muniba Waqar <muniba.wq@gmail.com>
2021-01-31 17:26:17 -0700

    Making a ajax request w/ jQuery  - creating req.body

--- helmetAndOthers.js
+++ helmetAndOthers.js
@@ -3,11 +3,16 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+// express.json and urlencoded are the two pieces of middlware that create 'req.body'
+// these two middlwares are very important because they will:
+// take any type of data, pass it for you and give it to you in json format which is what you probably are going to want
 app.use(express.json());
-app.use(urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-app.post("ajax", (req, res) => {
-  console.log(req);
+app.post("/ajax", (req, res) => {
+  //   console.log(req);
+  //   console.log(req.headers);
+  console.log(req.body);
   res.send("test");
 });
 
